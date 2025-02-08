b4fcec31a56ea31fed109859081b14664eb826b4
d25 <oladokundara@gmail.com>
2025-02-08 05:26:06 -0800

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com', 'imgur.com', 'i.imgur.com', 'lavender-impossible-kite-440.mypinata.cloud'], // Add imgur.com as an allowed domain
+  },
 };
 
 export default nextConfig;
